c3c515ace265591586cf03b38b634a8ee8561c8f
Victor Amaral <tito.victor@hotmail.com>
2021-03-23 15:03:45 -0300

    Page Perfil do cliente e Page de Categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CategoriaService } from '../services/domain/categoria.service';
+import { ErrorInterceptorProvider } from '../interceptors/erro.interceptor';
+import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage,
-    ListPage
+    MyApp
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CategoriaService,
+    ErrorInterceptorProvider,
+    AuthService,
+    StorageService,
+    ClienteService
   ]
 })
 export class AppModule {}
---
+++ src/config/api.config.ts
@@ -0,0 +1,5 @@
+export const API_CONFIG ={
+    baseURL :"http://localhost:8585",
+    bucketBaseURL :"https://app-lojinha-virtual.s3-sa-east-1.amazonaws.com"
+
+}
